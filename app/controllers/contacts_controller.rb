ab2f52fb22a9d01338686adecd15623a105db76d
Fanny Roseau <fannyroseau@hotmail.com>
2015-05-28 01:48:28 +0200

    fix bug contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:notice] = "Votre message a bien été envoyé!"
-      redirect_to new_contact_path
+      redirect_to root_path
     else
       render :new
     end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,6 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :subject, presence: true
-  validates :answered, presence: true
-
 end
 
 
