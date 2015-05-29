13ccbb6c753c4adfbb0fb5b83beae9e0f558f7a0
Fanny Roseau <fannyroseau@hotmail.com>
2015-05-29 14:13:52 +0200

    correct home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @articles = Article.all.order("publication DESC").paginate(:page => params[:page], :per_page => 4, total_entries: 20)
+    @articles = @articles.uniq{|article| article.id}
   end
 
 end
