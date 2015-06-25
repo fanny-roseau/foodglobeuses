a5b5483852165231a4233a36ceb1b677416455a3
Fanny Roseau <fannyroseau@hotmail.com>
2015-06-25 22:51:02 +0200

    blabl

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -53,7 +53,7 @@ class RestaurantsController < ApplicationController
   end
 
   def paris
-    @restaurants = Restaurant.where(city: "Paris").paginate(:page => params[:page], :per_page => 9, total_entries: 25)
+    @restaurants = Restaurant.where(city: "Paris").paginate(:page => params[:page], :per_page => 9, total_entries: 19)
   end
 
   def piana
